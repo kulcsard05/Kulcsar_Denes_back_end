c819abb05593fafe97f1eb8069ff1eb0230c7f47
kulcsard <kulcsdenes005@gmail.com>
2026-03-04 09:39:56 +0100

    Add project files.

---
+++ Kulcsar_Denes_back_end/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Kulcsar_Denes_back_end.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kulcsar_Denes_back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetCategories()
+        {
+            try
+            {
+                using (var cx=new LibrarydbContext())
+                {
+                    var response=cx.Categories.Select(x=>new {x.CategoryId,x.CategoryName,books=x.Books.ToList()}).ToList();
+                    return Ok(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Kulcsar_Denes_back_end/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kulcsar_Denes_back_end.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
